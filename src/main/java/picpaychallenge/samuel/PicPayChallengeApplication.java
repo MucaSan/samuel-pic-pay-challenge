36ca67170a1c35d442ff8f4b3bf47f48ae5e284f
svillar <villar.mucasan@gmail.com>
2024-03-25 23:28:42 -0300

    Initial commit

---
+++ src/main/java/picpaychallenge/samuel/PicPayChallengeApplication.java
@@ -0,0 +1,14 @@
+package picpaychallenge.samuel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicPayChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicPayChallengeApplication.class, args);
+
+	}
+
+}
